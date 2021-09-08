ccbeadd31cda1d321296114db0ed1f8fa9364a28
ken3986 <naka_ken8686@yahoo.co.jp>
2021-09-08 21:31:24 +0900

    更新確認機能を追加

--- functions.php
+++ functions.php
@@ -5,3 +5,12 @@ if ( !defined( 'ABSPATH' ) ) exit;
 add_editor_style();
 
 //以下に子テーマ用の関数を書く
+
+  // 更新の確認
+  require 'lib/plugin-update-checker/plugin-update-checker.php';
+  $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/ken3986/cocoon-child-ane-portfolio',
+    __FILE__,
+    'cocoon-child-ane-portfolio'
+  );
+  $myUpdateChecker -> getVcsApi() -> enableReleaseAssets();
