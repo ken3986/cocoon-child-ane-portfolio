5f03457ce50cff7c6fff47d8bed7ab8caa6e587a
ken3986 <naka_ken8686@yahoo.co.jp>
2021-09-18 19:31:20 +0900

    アーカイブページを作成

--- functions.php
+++ functions.php
@@ -14,3 +14,16 @@ add_editor_style();
     'cocoon-child-ane-portfolio'
   );
   $myUpdateChecker -> getVcsApi() -> enableReleaseAssets();
+
+
+/* 投稿アーカイブページの作成 */
+function post_has_archive( $args, $post_type ) {
+
+	if ( 'post' == $post_type ) {
+		$args['rewrite'] = true;
+		$args['has_archive'] = 'works'; //任意のスラッグ名
+	}
+	return $args;
+
+}
+add_filter( 'register_post_type_args', 'post_has_archive', 10, 2 );
