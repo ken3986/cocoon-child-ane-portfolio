e9c68ba5c23f3b0e6a4eb6ce797495021ef120fd
ken3986 <naka_ken8686@yahoo.co.jp>
2021-11-16 22:45:26 +0900

    画像の自動リサイズ設定を無制限に変更

--- functions.php
+++ functions.php
@@ -6,14 +6,14 @@ add_editor_style();
 
 //以下に子テーマ用の関数を書く
 
-  // 更新の確認
-  require 'lib/plugin-update-checker/plugin-update-checker.php';
-  $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-    'https://github.com/ken3986/cocoon-child-ane-portfolio',
-    __FILE__,
-    'cocoon-child-ane-portfolio'
-  );
-  $myUpdateChecker -> getVcsApi() -> enableReleaseAssets();
+// 更新の確認
+require 'lib/plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+  'https://github.com/ken3986/cocoon-child-ane-portfolio',
+  __FILE__,
+  'cocoon-child-ane-portfolio'
+);
+$myUpdateChecker -> getVcsApi() -> enableReleaseAssets();
 
 
 /* 投稿アーカイブページの作成 */
@@ -27,3 +27,6 @@ function post_has_archive( $args, $post_type ) {
 
 }
 add_filter( 'register_post_type_args', 'post_has_archive', 10, 2 );
+
+// 画像の自動リサイズ設定を2560px→無制限に変更
+add_filter( 'big_image_size_threshold', '__return_false' );
